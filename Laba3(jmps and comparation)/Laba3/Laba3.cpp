50761e4575e6c42cf8e131a18ed976e72bb048ba
LionVas <165137090+LionVas@users.noreply.github.com>
2024-11-28 03:42:43 +1000

    Add files via upload

---
+++ Laba3(jmps and comparation)/Laba3/Laba3.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <stdio.h>
+using namespace std;
+int main()
+{
+    int a, x;
+    char a1;
+    cout << " Enter 'a' \n";
+    cin >> a;
+    cout << "Enter 'x' \n";
+    cin >> x;
+    _asm {
+        mov ebx, a;
+        mov eax, x;
+        mov edx, 2;
+        cmp al, 2;
+        jg L1;
+        mul edx;
+        add eax, 1;
+        mov ecx, eax;
+        mov eax, x;
+        jmp L2;
+        L1:
+        mul edx;
+        add eax, ebx;
+        mov ecx, eax;
+        mov eax, x;
+        L2:
+        cmp al, 0;
+        jg L3;
+        sub ebx, 1;
+        mov edx, ebx;
+        jmp L4;
+        L3:
+        add eax, 1;
+        mov edx, eax;
+        L4:
+        sub ecx, edx;
+        mov x, ecx;
+    }
+    printf("%d", x);
+}
+
+
---
+++ Laba4(String methods)/Laba4.cpp
@@ -0,0 +1,76 @@
+﻿#include <stdio.h>
+#include "conio.h"
+#include <iostream>
+#include<cstring>
+using namespace std;
+int main() {
+	char source[32] = "nsjg soplea dsbfs ads";
+	char dest[32] = "dfsasdfsf";
+	char ch1, ch2;
+	int slon;
+	int len;
+	len = strlen(source);
+	_asm {
+		lea esi, source;
+		lea edi, source;
+	ag:
+		mov ecx, len;
+		mov al, ' ';
+		mov ebx, esi;
+		repne scasb;
+		dec edi;
+	/*	mov al, [edi]; юзлес строчка, пробел и так в al*/
+		mov ecx, edi;
+		sub ecx, ebx;
+		mov eax, len;
+		sub eax, ecx;
+		mov len, eax;
+		mov eax, ecx;
+		mov edx, 0;
+		mul ecx;
+		mov ecx, eax;
+		mov edx, esi;
+		/*mov slon, ecx;*/
+	rp:
+		mov edi, esi;
+		inc edi; 
+	pr:
+		mov al, [esi];
+		mov bl, [edi];
+		cmp cx, 0;
+		jle fin;
+		rep cmps
+		jle pr;
+		jg sw;
+	sw:
+		/*mov al, [esi];
+		mov bl, [edi];*/
+		dec esi;
+		dec edi;
+		cmp[edi], ' '
+		je fin;
+		mov al, [esi];
+		mov bl, [edi];
+		mov [esi], bl;
+		mov [edi], al;
+		mov esi, edx;
+		jmp rp;
+	fin:
+		inc edi;
+		cmp [edi],' '
+		je rfin
+		cmp [edi], 0
+		je rfin;
+		mov esi, edi
+		mov al, [edi];
+		//mov ch1, al;
+		jmp ag;
+		
+	}
+	rfin:
+	cout << "----------" << endl;
+	cout << ch1 << " " << ch2 << " " << slon<< endl;
+	for (char c : source) {
+		cout << c;
+	}
+}
